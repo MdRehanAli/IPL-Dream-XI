c444770d84cb928b9e0a4a74368c1f56b7313148
Md Rehan Ali <rehanali21d@gmail.com>
2025-09-26 22:00:19 +0600

    Added Json Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 
 import './App.css'
+import navImg from './assets/logo.png'
 
 function App() {
 
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar max-w-7xl mx-auto">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">
+            <img className='w-[60px] h-[60px]' src={navImg} alt="" />
+          </a>
         </div>
         <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
-          </button>
+          <span className='mr-2'>6000000000</span>
+          <span>coin</span>
         </div>
       </div>
     </>
